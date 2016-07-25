cf19a42f999b146da1ab8c479515c0a3c4aedda8
790057066@qq.com <790057066@qq.com>
2016-07-25 10:15:15 +0800

    Initial Commit

---
+++ SZKRoundScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZKRoundScrollView
+//
+//  Created by sunzhaokai on 16/7/25.
+//  Copyright © 2016年 孙赵凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SZKRoundScrollViewTests/SZKRoundScrollViewTests.m
@@ -0,0 +1,39 @@
+//
+//  SZKRoundScrollViewTests.m
+//  SZKRoundScrollViewTests
+//
+//  Created by sunzhaokai on 16/7/25.
+//  Copyright © 2016年 孙赵凯. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SZKRoundScrollViewTests : XCTestCase
+
+@end
+
+@implementation SZKRoundScrollViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
